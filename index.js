1bebde80446ac98d8ea233a236eeb421af97f54e
Bentil Okyere Boadu <bentil.boadu@amalitech.org>
2023-02-07 11:17:37 +0000

    Step 10: Descriptor for Applications

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-sap.ui.define([
-    // "sap/ui/core/mvc/XMLView",
-	"sap/ui/core/ComponentContainer"
-], function (ComponentContainer) {
-	"use strict";
-    // XMLView.create({
-	// 	viewName: "sap.ui.demo.walkthrough.view.App"
-	// }).then(function (oView) {
-	// 	oView.placeAt("content");
-	// });
+// sap.ui.define([
+//     "sap/ui/core/mvc/XMLView",
+// 	"sap/ui/core/ComponentContainer"
+// ], function (ComponentContainer) {
+// 	"use strict";
+//     XMLView.create({
+// 		viewName: "sap.ui.demo.walkthrough.view.App"
+// 	}).then(function (oView) {
+// 		oView.placeAt("content");
+// 	});
 
-	new ComponentContainer({
-		name: "sap.ui.demo.walkthrough",
-		settings : {
-			id : "walkthrough"
-		},
-		async: true
-	}).placeAt("content");
+// 	new ComponentContainer({
+// 		name: "sap.ui.demo.walkthrough",
+// 		settings : {
+// 			id : "walkthrough"
+// 		},
+// 		async: true
+// 	}).placeAt("content");
 
-});
+// });
