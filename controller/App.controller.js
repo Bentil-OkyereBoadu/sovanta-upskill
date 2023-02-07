9fa8be38a3a609044f7f76ceefca4a74e2a6854b
Bentil Okyere Boadu <bentil.boadu@amalitech.org>
2023-02-07 14:35:00 +0000

    Step 15: Nested Views

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,16 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/ui/core/mvc/Controller"
+ ], function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello : function () {
-            // read msg from i18n model
-         const bundle = this.getView().getModel("i18n").getResourceBundle();
-         const recipient = this.getView().getModel().getProperty("/recipient/name");
-         const msg = bundle.getText("helloMsg", [recipient]);
-         // show message
-         MessageToast.show(msg);
-        }
+        
     });
  });
